1e513461a8d0b450248db1e4b1113432c833b5b2
luo.shuidong <lsd751@qq.com>
2016-02-01 17:17:01 +0800

    x

---
+++ JsonBatch/JsonInfoGetter.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Codeplex.Data;
+
+namespace JsonBatch
+{
+    class JsonInfoGetter
+    {
+        public static void getFirstFrameXY(string jsonContent, out int x, out int y)
+        {
+            var json = DynamicJson.Parse(jsonContent);
+            var node = json["mc"];
+            dynamic firstNode = null;
+            foreach (KeyValuePair<string, dynamic> item in node)
+            {
+                //Console.WriteLine(item.Key + ":" + item.Value);
+                firstNode = item.Value;
+                break;
+            }
+
+            var frames = firstNode["frames"];
+            var frame = frames[0];
+            x = (int)frame["x"];
+            y = (int)frame["y"];
+            //foreach (dynamic item in frames) {
+            //    return item["x"];
+            //}
+            //Logger.debug("failed when getFirstFrameX.");
+            //return 0;
+        }
+
+        public static string modifyJson(int dltX, int dltY, string text)
+        {
+            var json = DynamicJson.Parse(text);
+            var node = json["mc"];
+            dynamic firstNode = null;
+            foreach (KeyValuePair<string, dynamic> item in node)
+            {
+                //Console.WriteLine(item.Key + ":" + item.Value);
+                firstNode = item.Value;
+                break;
+            }
+
+            var frames = firstNode["frames"];
+            foreach (dynamic frame in frames) {
+                frame["x"] -= dltX;
+                frame["y"] -= dltY;
+            }
+
+            var jsonstring = json.ToString();
+            return jsonstring;
+        }
+    }
+}
---
+++ JsonBatch/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonBatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (0 == args.Length) {
+                Logger.debug("输入参数(原始json)(修改后的json)(待覆盖输出的json)");
+                return;
+            }
+            
+            string originJson = args[0];
+            string modifyedJson = args[1];
+            string outputJson = args[2];
+
+            /*
+            string originJson = @"E:\_git\newlegend\resource\assets\image\demo\role1\r1_2r.json";
+            string modifyedJson = @"E:\_git\newlegend\resource\assets\image\demo\role1\r1_3a.json";
+            string outputJson = @"E:\_git\r1_4s.json";
+            */
+
+            string text = System.IO.File.ReadAllText(originJson);
+            int originX = 0, originY = 0;
+            JsonInfoGetter.getFirstFrameXY(text, out originX, out originY);
+
+            string text2 = System.IO.File.ReadAllText(modifyedJson);
+            int modifyX, modifyY;
+            JsonInfoGetter.getFirstFrameXY(text2, out modifyX,  out modifyY);
+
+            int dltX = originX - modifyX;
+            int dltY = originY - modifyY;
+
+            string text3 = System.IO.File.ReadAllText(outputJson);
+            string resultJson = JsonInfoGetter.modifyJson(dltX, dltY, text3);
+            System.IO.File.WriteAllText(outputJson, resultJson);
+            return;
+
+        }
+    }
+}
